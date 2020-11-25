ae4d3aa8d51924aaa9106637333588897952a43a
김병권 <ssamtkwon@naver.com>
2020-11-25 22:40:03 +0900

    changed various things for production

--- KSABooks/settings.py
+++ KSABooks/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_7y5$z$@^96+5k1yg8-c0*^(a!9o76)or#!!f^=!o!ze*ck=2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

1b9f619e290c133eaad32126b77ab7323d93dbf9
김병권 <ssamtkwon@naver.com>
2020-11-27 08:06:27 +0900

    tried hiding SECRET_KEY

--- KSABooks/settings.py
+++ KSABooks/settings.py
@@ -15,7 +15,6 @@ import os
 import django_heroku
 import dj_database_url
 import dotenv
-from .setting_secret import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY', secret_key)
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

323d4b8eb54245ad2f4ff42d073aecc4f7127bdc
김병권 <ssamtkwon@naver.com>
2020-11-28 12:09:24 +0900

    added github link and hid the email password

--- KSABooks/settings.py
+++ KSABooks/settings.py
@@ -139,7 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "ksabooks1@gmail.com"
-EMAIL_HOST_PASSWORD = "QwTTsHtcYekn3Vn"
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 django_heroku.settings(locals())
